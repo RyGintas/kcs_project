760baa56a617ceac78a55fd153bcb14873f95aec
Aidis Stukas <aidiss@gmail.com>
2019-05-19 22:00:10 +0300

    Fix unicode escape error, rename files and add working bootstrap html

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
+import os
+import urllib.request
+
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
 from flask import Flask
 
-UPLOAD_FOLDER = 'C:\kcs_project\uploads'
+UPLOAD_FOLDER = 'C:/kcs_project/uploads'
 ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+        # check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No file selected for uploading')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			flash('File(s) successfully uploaded')
+			return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
