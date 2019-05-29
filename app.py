205e8e4ac4dca8a652a73240d5abbdd295fe5c69
Aidis Stukas <aidiss@gmail.com>
2019-05-29 20:58:15 +0300

    Fix excel writing

--- app.py
+++ app.py
@@ -1,55 +1,65 @@
 import os
 import camelot
 from openpyxl import load_workbook
-from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from flask import (
+    Flask,
+    request,
+    redirect,
+    url_for,
+    render_template,
+    send_from_directory,
+)
 from werkzeug.utils import secure_filename
+from flask import send_from_directory
 
-UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
-DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
-ALLOWED_EXTENSIONS = set(['pdf'])
-
-app = Flask(__name__, static_url_path='/static')
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/uploads/"
+DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/downloads/"
+ALLOWED_EXTENSIONS = set(["pdf"])
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-#app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+
+app = Flask(__name__, static_url_path="/static")
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+# app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 # limit upload size upto 8mb
-app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
+app.config["MAX_CONTENT_LENGTH"] = 8 * 1024 * 1024
+
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part")
             return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
+        file = request.files["file"]
+        if file.filename == "":
+            flash("No selected file")
             return redirect(request.url)
+
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            prepear_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return return_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return render_template('home.html')
+            file_name_pdf = secure_filename(file.filename)
+            file_path_pdf = os.path.join(app.config["UPLOAD_FOLDER"], file_name_pdf)
 
-def prepear_file(path):
-    tables = camelot.read_pdf(path)
-    t1 = tables[0].df
-    t1= t1.T.set_index(0).T
-    t1.drop(['Comments', 'Ordered by' ], axis=1, inplace=True)
-    t1.insert(0, "Warehouse", "V0020LV")
-    t1.to_excel(os.path.join(app.config['UPLOAD_FOLDER'], 'Exported_file.xlsx'))
+            file_name_xlsx = file_name_pdf.replace("pdf", "xlsx")
+            file_path_xlsx = os.path.join(app.config["UPLOAD_FOLDER"], file_name_xlsx)
 
- 
-def return_file(path):
-    file_name = 'Exported_file.xlsx'
-    wb = load_workbook('Exported_file.xlsx')
-    wb.save(file_name)
-    from flask import send_from_directory
-    return send_from_directory(app.config['UPLOAD_FOLDER'], file_name, as_attachment=True)
+            file.save(file_path_pdf)
+
+            tables = camelot.read_pdf(file_path_pdf)
+            table = tables[0].df
+            table = table.T.set_index(0).T
+            table.drop(["Comments", "Ordered by"], axis=1, inplace=True)
+            table.insert(0, "Warehouse", "V0020LV")
+
+            table.to_excel(file_path_xlsx)
+
+            return send_from_directory(
+                app.config["UPLOAD_FOLDER"], file_name_xlsx, as_attachment=True
+            )
+    return render_template("home.html")
 
 app.run(debug=True)
